7add85c4e27ba67011bb7033c9bc8084f2e7ad9d
uflc <randomutate@gmail.com>
2021-03-07 01:01:40 +0900

    아이템 기본 셋업

---
+++ Source/GProject/Public/GPItem.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "GPItem.generated.h"
+#include "Items/GPItemData.h"
+
+UCLASS()
+class GPROJECT_API AGPItem : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AGPItem();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void Use();
+
+	virtual void Loot(); //땅에서 인벤토리로 획득.
+	virtual void Drop(); //인벤토리에서 다시 땅으로. 
+	
+protected:
+	UGPItemData Data;
+
+	int Price;
+
+
+};
